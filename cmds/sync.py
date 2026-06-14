b476221bf51d68643969d09c85b9ed07bc414d97
epw1624 <emily.wood1624@gmail.com>
2026-06-14 13:51:18 -0700

    Add file sync command to CLI

--- cmds/sync.py
+++ cmds/sync.py
@@ -1,2 +1,49 @@
-def sync(dst: str):
-    print(f"Placeholder for syncing files to {dst}")
+import os
+from pathlib import Path
+import subprocess
+import typer
+
+def sync(src: str, dst: str):
+    src_dir = src / "src"
+    
+    if not src_dir.exists():
+        typer.echo(f"Source directory not found in Roverflake root: {src}")
+
+    typer.echo("Compiling roverflake project...")
+
+    colcon_cmd = [
+        "colcon", "build",
+        "--base-paths", str(src_dir),
+        "--build-base", str(src / "build"),
+        "--install-base", str(src / "install"),
+        "--symlink-install"
+    ]
+
+    # My laptop can't handle colcon builds with all cores and it only has 5
+    # So this is to limit the number of cores
+    # Should maybe remove when building on a better machine
+    env_vars = os.environ.copy()
+    env_vars["MAKEFLAGS"] = "-j3"
+
+    build_result = subprocess.run(colcon_cmd, env=env_vars)
+    if build_result.returncode != 0:
+        typer.echo("Compilation failed. File transfer aborted")
+        raise typer.Exit(code=1)
+    
+    DST_PATH = "/home/roverflake/"
+    typer.echo(f"Transferring files to {dst:DST_PATH}")
+
+    rsync_cmd = [
+        "rsync", "-azc",
+        "--delete",
+        "--stats",
+        str(src / "install"),
+        f"{dst}:{DST_PATH}"
+    ]
+
+    rsync_result = subprocess.run(rsync_cmd)
+    if rsync_result.returncode != 0:
+        typer.echo("Rsync file transfer failed")
+        raise typer.Exit(code=1)
+    
+    typer.echo("File transfer complete!")
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = typer.Typer(help="Rover Command Line Interface")
 sync_app = typer.Typer()
 
 @sync_app.callback(invoke_without_command=True)
-def sync(context: typer.Context, dst: str = typer.Argument(..., help="Sync files to the rover/comms base onboard computers")):
+def sync(context: typer.Context, src: str = typer.Argument(..., help="Sync source (Roverflake root)"), dst: str = typer.Argument(..., help="Sync destination")):
     if context.invoked_subcommand is None:
-        cmds.sync(dst)
+        cmds.sync(src, dst)
 
 app.add_typer(sync_app, name="sync")
 
