b8bccaf052fd717a390bc084c9c1806e578b26eb
BenUBC <ben5@student.ubc.ca>
2026-07-18 11:29:30 -0700

    adding some things that id like to try tomorrow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import typer
 
 import cmds
@@ -7,9 +9,15 @@ app = typer.Typer(help="Rover Command Line Interface")
 sync_app = typer.Typer()
 
 @sync_app.callback(invoke_without_command=True)
-def sync(context: typer.Context, src: str = typer.Argument(..., help="Sync source (Roverflake root)"), dst: str = typer.Argument(..., help="Sync destination")):
+def sync(
+    context: typer.Context, 
+    src_root: Path = typer.Argument(..., help="Sync source (Roverflake root)"), 
+    dst_root: Path = typer.Argument("~/Roverflake2", help="Sync remote destination (Roverflake root)"),
+    remote_host: str = typer.Argument("rv@192.168.1.4", help="Remote host for syncing"), 
+    build: bool = typer.Option(True, "--build", "-b", help="Build the project before syncing")
+):
     if context.invoked_subcommand is None:
-        cmds.sync(src, dst)
+        cmds.sync(src_root, dst_root, remote_host=remote_host, build=build)
 
 app.add_typer(sync_app, name="sync")
 
