369a2fcc32dd78971a3075b76457ac69d06701f3
BenUBC <ben5@student.ubc.ca>
2026-07-19 15:05:05 -0700

    sorry for commit spam

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def sync(args):
     )
 
 sync_parser = sync_app.add_parser("sync")
-sync_parser.add_argument("--src-root", default="/home/rv/Roverflake2/", help="Sync source (Roverflake root)")
+sync_parser.add_argument("--src-root", help="Sync source (Roverflake root)", required=True)
 sync_parser.add_argument("--dst-root", default="/home/rv/Roverflake2/", help="Sync remote destination (Roverflake root)")
 sync_parser.add_argument("--remote-host", default="rv@192.168.1.4", help="Remote host for syncing")
 sync_parser.add_argument(
