cc73c517fac69e65b671e9ad38575b1012f9b19b
BenUBC <ben5@student.ubc.ca>
2026-07-19 14:13:34 -0700

    switch to argument

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ app = typer.Typer(help="Rover Command Line Interface")
 
 sync_app = typer.Typer()
 
-@sync_app.callback(invoke_without_command=True)
+@sync_app.callback()
 def sync(
     context: typer.Context, 
     src_root: Path = typer.Argument("~/Roverflake2", help="Sync source (Roverflake root)"), 
     dst_root: Path = typer.Argument("~/Roverflake2", help="Sync remote destination (Roverflake root)"),
     remote_host: str = typer.Argument("rv@192.168.1.4", help="Remote host for syncing"), 
-    build: bool = typer.Option(True, "--build", "-b", help="Build the project before syncing"),
-    packages: Optional[str] = typer.Option(
+    build: bool = typer.Argument(True, help="Build the project before syncing"),
+    packages: Optional[str] = typer.Argument(
         None, 
-        "--packages", "-p", 
         help="Space-separated list of packages to transfer and build (e.g., 'arm_control drive_control')"
     )
 ):
