6c0ac6c19c553cfa47d2fe1efd912c18ec8735e7
BenUBC <ben5@student.ubc.ca>
2026-07-19 14:49:01 -0700

    a

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ def sync(args):
         args.src_root,
         args.dst_root,
         remote_host=args.remote_host,
-        build=args.build,
+        build=not args.no_build,
         packages=args.packages,
     )
 
 sync_parser = sync_app.add_parser("sync")
-sync_parser.add_argument("--src_root", type=str, default="~/Roverflake2/", help="Sync source (Roverflake root)")
-sync_parser.add_argument("--dst_root", type=str, default=str(Path.home() / "Roverflake2"), help="Sync remote destination (Roverflake root)")
-sync_parser.add_argument("--remote_host", default="rv@192.168.1.4", help="Remote host for syncing")
-sync_parser.add_argument("--build", type=bool, default=True, help="Build the project before syncing")
+sync_parser.add_argument("--src-root", type=str, default="~/Roverflake2/", help="Sync source (Roverflake root)")
+sync_parser.add_argument("--dst-root", type=str, default="~/Roverflake2/", help="Sync remote destination (Roverflake root)")
+sync_parser.add_argument("--remote-host", default="rv@192.168.1.4", help="Remote host for syncing")
+sync_parser.add_argument(
+    "--no-build",
+    action="store_true",
+    help="Disable building the project before syncing",
+)
 sync_parser.add_argument("--packages", default=None, help="Space-separated list of packages to transfer and build (e.g., 'arm_control drive_control')")
 sync_parser.set_defaults(func=sync)
 
