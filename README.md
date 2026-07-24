aeadc2199055cbfd0b5cca36e990646448e9bb13
epw1624 <emily.wood1624@gmail.com>
2026-07-23 19:15:20 -0700

    Add Installation section to README

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+# `rovercli`
+A multi-purpose CLI for UBC Rover operations. Add commands as needed
+
+# Installation
+`rovercli` has no dependencies outside the Python standard library. To use it:
+1. Clone this repo
+2. From the root of the repo, run `python main.py [COMMAND] [ARGS]`
+3. **[OPTIONAL BUT RECOMMENDED]** To install the CLI on the system root and run commands from anywhere using `rovercli [COMMAND] [ARGS]`:
+   1. Ensure you are in the root of the repo
+   2. `chmod +x main.py`
+   3. `ln -s "$(pwd)/main.py" ~/.local/bin/rovercli`
+   4. Verify that the symlink worked by running `rovercli`
+
+# Commands
+## `sync`
+Sync files between devices on the UBC Rover network, without unecessary copying of files that haven't changed.
+
+### Usage
+
+```
+python main.py sync [SOURCE ROOT] [DEST ROOT] [DEST ADDRESS]
+```
+For example:
 ```
 python main.py sync ~/RoverFlake2 ~/RoverFlake2 rv@192.168.1.4
 ```
--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from pathlib import Path
-import argparse
+#!/usr/bin/env python3
 
+import argparse
 import cmds
 
-app = argparse.ArgumentParser(description="Rover Command Line Interface")
-sync_app = app.add_subparsers(dest="command")
+def main():
+    app = argparse.ArgumentParser(description="Rover Command Line Interface")
+    sync_app = app.add_subparsers(dest="command")
 
-def sync(args):
-    cmds.sync(
-        args.src_root,
-        args.dst_root,
-        remote_host=args.remote_host,
-        build=not args.no_build,
-        packages=args.packages,
+    def sync(args):
+        cmds.sync(
+            args.src_root,
+            args.dst_root,
+            remote_host=args.remote_host,
+            build=not args.no_build,
+            packages=args.packages,
+        )
+
+    sync_parser = sync_app.add_parser("sync")
+    sync_parser.add_argument("--src-root", help="Sync source (Roverflake root)", required=True)
+    sync_parser.add_argument("--dst-root", default="/home/rv/Roverflake2/", help="Sync remote destination (Roverflake root)")
+    sync_parser.add_argument("--remote-host", default="rv@192.168.1.4", help="Remote host for syncing")
+    sync_parser.add_argument(
+        "--no-build",
+        action="store_true",
+        help="Disable building the project before syncing",
     )
+    sync_parser.add_argument("--packages", default=None, help="Space-separated list of packages to transfer and build (e.g., 'arm_control drive_control')")
+    sync_parser.set_defaults(func=sync)
 
-sync_parser = sync_app.add_parser("sync")
-sync_parser.add_argument("--src-root", help="Sync source (Roverflake root)", required=True)
-sync_parser.add_argument("--dst-root", default="/home/rv/Roverflake2/", help="Sync remote destination (Roverflake root)")
-sync_parser.add_argument("--remote-host", default="rv@192.168.1.4", help="Remote host for syncing")
-sync_parser.add_argument(
-    "--no-build",
-    action="store_true",
-    help="Disable building the project before syncing",
-)
-sync_parser.add_argument("--packages", default=None, help="Space-separated list of packages to transfer and build (e.g., 'arm_control drive_control')")
-sync_parser.set_defaults(func=sync)
-
-if __name__ == "__main__":
     args = app.parse_args()
     if hasattr(args, "func"):
         args.func(args)
     else:
         app.print_help()
+
+if __name__ == "__main__":
+    main()
