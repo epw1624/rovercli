88f716a6da80414dc89f35a669549bcb56e29201
BenUBC <ben5@student.ubc.ca>
2026-07-29 17:00:21 -0700

    update jet sync

--- sync_jetson.bash
+++ sync_jetson.bash
@@ -1,1 +1,1 @@
-python3 main.py sync --remote-host rv@192.168.1.5 --packages arm_hardware_interface cameras_cpp drive_control ptz_cam rover_arm_common rover_gnss rover_launchers rover_msgs rover_utils rover_manager
+python3 main.py sync --remote-host rv@192.168.1.5 --packages cameras_cpp ptz_cam rover_launchers rover_msgs rover_utils rover_manager
