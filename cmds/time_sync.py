a447a647a88ef74d7195121e454545ee36d10993
BenUBC <ben5@student.ubc.ca>
2026-08-03 11:40:44 -0700

    fix

--- cmds/time_sync.py
+++ cmds/time_sync.py
@@ -3,7 +3,9 @@ import subprocess
 def time_sync(remote_host: str = "rover"):
     print(f"Synchronizing time with remote host: {remote_host}")
     try:
-        subprocess.run(["ssh", remote_host, "sudo", "-S", "date", "--set='@$(date -u +%s)'"], check=True, input=input("Enter password for sudo: ").encode(), capture_output=True)
+        date_output = subprocess.check_output(["date", "-u", "+%s"]).decode().strip()
+        
+        subprocess.run(["ssh", remote_host, "sudo", "-S", "date", f"--set={date_output}"], check=True, input=input("Enter password for sudo: ").encode(), capture_output=True)
         print("Time synchronization successful.")
     except subprocess.CalledProcessError as e:
         print(f"Time synchronization failed: {e}")
