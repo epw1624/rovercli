61b33b61bbca142dbe3f4b56c5119a9e961caf51
BenUBC <ben5@student.ubc.ca>
2026-08-03 11:47:20 -0700

    get the time after pw input

--- cmds/time_sync.py
+++ cmds/time_sync.py
@@ -3,9 +3,10 @@ import subprocess
 def time_sync(remote_host: str = "rover"):
     print(f"Synchronizing time with remote host: {remote_host}")
     try:
+        pw = input("Enter password for sudo: ").encode()
         date_output = subprocess.check_output(["date", "-u", "+%s"]).decode().strip()
         
-        subprocess.run(["ssh", remote_host, "sudo", "-S", "date", f"--set='@{date_output}'"], check=True, input=input("Enter password for sudo: ").encode(), capture_output=True)
+        subprocess.run(["ssh", remote_host, "sudo", "-S", "date", f"--set='@{date_output}'"], check=True, input=pw, capture_output=True)
         print("Time synchronization successful.")
     except subprocess.CalledProcessError as e:
         print(f"Time synchronization failed: {e}")
