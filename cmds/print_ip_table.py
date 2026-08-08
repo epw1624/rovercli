5327689cdb713acaf7ae71bb85ba8a3165bb8e29
Roozki <zawadzkirowan@gmail.com>
2026-08-07 19:21:17 -0700

    add rowan

--- cmds/print_ip_table.py
+++ cmds/print_ip_table.py
@@ -8,7 +8,8 @@ DISH = ["Dish", "192.168.1.20", "dish"]
 BEN = ["Ben's laptop", "192.168.1.55", "ben"]
 PTZ = ["PTZ Camera", "192.168.1.88", "ptz"]
 RELAY_PI = ["Relay Pi", "192.168.1.52", "relay_pi"]
-ALL_DEVICES = [JETSON, ONBOARD, BASE, COMMS_PI, RELAY, ONBOARD_ANTENNA, DISH, BEN, PTZ, RELAY_PI]
+ROWAN = ["Rowan's Laptop", "192.168.1.40", "rowan"]
+ALL_DEVICES = [JETSON, ONBOARD, BASE, COMMS_PI, RELAY, ONBOARD_ANTENNA, DISH, BEN, PTZ, RELAY_PI, ROWAN]
 
 def print_ip_table():
     print("To create an alias, run sudo nano /etc/hosts and paste the following lines:")
