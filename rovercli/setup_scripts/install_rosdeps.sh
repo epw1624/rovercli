8254c9f74871d776ad19cf309fdfb984e55224c3
BenUBC <ben5@student.ubc.ca>
2026-09-06 09:53:18 -0700

    rosdep ignore rviz

--- rovercli/setup_scripts/install_rosdeps.sh
+++ rovercli/setup_scripts/install_rosdeps.sh
@@ -1,4 +1,4 @@
 cd "$ROVERFLAKE_ROOT"
 sudo rosdep init
 rosdep update
-rosdep install --from-paths src --ignore-src -r --skip-keys="serial moteus_msgs" -y --rosdistro $ROS_DISTRO
+rosdep install --from-paths src --ignore-src -r --skip-keys="serial moteus_msgs" -y --rosdistro $ROS_DISTRO --skip-keys="rviz rviz2 "
